59631244f61b66a8a5fc9a0c845200aa3bd67556
Azumi Callanan <azucm@hotmail.com>
2018-01-21 17:42:55 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,7 +4,11 @@ function paragraphSelector(){
 }
 
 function lastImageSelector(){
+<<<<<<< HEAD
   return $('img:last');
+=======
+  return 
+>>>>>>> 71b83abd6f04db4d2bb32104746b750b8ce00826
 }
 
 function ninjaBabySelector(){
@@ -16,5 +20,9 @@ function divSelector(){
 }
 
 function firstListItem(){
+<<<<<<< HEAD
   return $('ul li:first-child');
+=======
+  return $('ul').firstChild('#pic-list');
+>>>>>>> 71b83abd6f04db4d2bb32104746b750b8ce00826
 }
